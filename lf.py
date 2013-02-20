8b7761b1da0d46a50a66d1b32e10e39225aefeea
Geoff <Geoff@Geoffs-MacBook.local>
2013-02-19 21:32:43 -0500

    iterate through folders

--- lf.py
+++ lf.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python
 from sys import argv
 import os
+import itertools
 
 #Set up folders and sub-folders
-mainfolders = ["01. Specifications","02. References","03. Assets","04. Documents"]
-
-assets = ["01. PSDs et. al.","02. Rendered Images","03. Scripts"]
-
-documents = ["01. Text","02. Spreadsheets","03. Presentations","04. Other"]
+mainfolders = [["01. Static","02. Animate","03. Assets"],
+                ["01. MM&M","02. PM360","03. MedAdNews"],
+                ["01. A", "02. B"],
+                ["01. Text","02. Spreadsheets","03. Presentations","04. Other"]]
 
 # Check for number of arguments
 # Need at least one - starting folder name
 
 if len(argv) == 1:
     print "Please Enter a folder name:\n"
-    path = raw_input('> ')
+    project = raw_input('> ')
 else:
-    path = argv[1]
+    project = argv[1]
 
 try:
-    os.makedirs(path)
-    for i, b in enumerate(mainfolders):
-        os.makedirs(path+"/"+mainfolders[i])
+    os.makedirs(project)
+    for item in itertools.product(*mainfolders):
+        os.makedirs(project+"/"+os.path.join(*item))
 except OSError:
     raise Exception("Folder Exists!")
